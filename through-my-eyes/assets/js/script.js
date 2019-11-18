5e2c6893954a2dc378f2950968a57598b31152d2
Abel <a.vanbeek@students.uu.nl>
2019-11-18 22:39:51 +0100

    Improved accessibility with anchor links to 'About me' page (at-my-eyes)

---
+++ through-my-eyes/assets/js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new SwapEyesCheckBoxHandler();
+});
+
+class SwapEyesCheckBoxHandler {
+
+    constructor () {
+        this.swapEyesCheckBox = document.getElementById('swap-eyes');
+        this.swapEyesCheckBox.addEventListener('change', e => this.HandleCheck(e));
+        this.HandlePageLoad()
+    }
+
+    HandleCheck (e) {
+        if (e.target.checked) {
+            HelperFunctions.setAnchor('at-my-eyes');
+        } else {
+            HelperFunctions.clearAnchor();
+        }
+    }
+
+    HandlePageLoad () {
+        if (HelperFunctions.getAnchor() === 'at-my-eyes') {
+            this.swapEyesCheckBox.checked = true;
+        }
+    }
+}
+
+class HelperFunctions {
+    
+    static getAnchor () {
+        return (document.URL.split('#').length > 1) ? document.URL.split('#')[1] : null;
+    }
+
+    static setAnchor (target) {
+        location.hash = target;
+    }
+
+    static clearAnchor () {
+        history.pushState("", document.title, window.location.pathname + window.location.search);
+    }
+}
